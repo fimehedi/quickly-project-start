b69a70a93890f879902c8e04bdc4786747251f7e
Fokrul Islam Mehedi <fokrulislammehedi2000@gmail.com>
2020-09-15 04:11:04 +0600

    Update .project-commands.sh

--- .project-commands.sh
+++ .project-commands.sh
@@ -3,14 +3,14 @@
 function project-create() {
     mkdir $1
     cd $1
-    python3 ~/.quick-project-create/project-create.py $1 -$2
+    python3 ~/.quick-project-start/project-create.py $1 -$2
 }
 
 function project-open() {
-    python3 ~/.quick-project-create/project-open.py $1 -$2
+    python3 ~/.quick-project-start/project-open.py $1 -$2
 }
 
 function project-remove() {
     rm -rf $1
-    python3 ~/.quick-project-create/project-remove.py $1 -$2
+    python3 ~/.quick-project-start/project-remove.py $1 -$2
 }
